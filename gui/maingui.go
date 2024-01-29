98672c4a9334e7a0aa92b6873e121cb6d3a6e4a4
lxg00n <lxg00n@waifu.club>
2024-01-29 17:48:35 -0300

    feat: starting to implement a file picker for selecting files for decoding/encoding

--- gui/maingui.go
+++ gui/maingui.go
@@ -1,8 +1,10 @@
 package gui
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -10,13 +12,31 @@ import (
 // Still don't know if I'm going to stick with fyne, but I guess I'll find out.
 func StartGui() {
 	appl := app.New()
-	win := appl.NewWindow("Test")
+	win := appl.NewWindow("ciphgoer")
 
-	hello := widget.NewLabel("Oi, teste")
+	win.Resize(fyne.NewSize(400, 400))
+	ab := widget.NewLabel("Selected file path will appear here.")
+
+	filePicker := func() {
+		dialog.ShowFileOpen(func(f fyne.URIReadCloser, err error) {
+			if err != nil {
+				dialog.NewError(err, win).Show()
+				return
+			}
+
+			if f == nil {
+				dialog.NewInformation("Error", "Action canceled by user", win).Show()
+				return
+			}
+
+			ab.SetText(f.URI().String())
+		}, win)
+	}
+
 	win.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Bem venido!")
+		ab,
+		widget.NewButton("Select a file", func() {
+			filePicker()
 		}),
 	))
 
